f4a146f9a5ba74d1fcb07090d8f49aac219a2387
Clasp <clasp2313@gmail.com>
2023-08-29 04:08:32 -0400

    First Commit

---
+++ carbon/src/app/page.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState } from 'react'; // Import useState
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
+export default function Home() {
+  const [audioLoaded, setAudioLoaded] = useState(false);
+
+  const handleAudioPlay = () => {
+    const audio = document.getElementById("save"); 
+    if (audioLoaded) {
+      audio.play();
+    } else {
+      audio.addEventListener('canplaythrough', () => {
+        audio.play();
+      });
+      audio.load();
+      setAudioLoaded(true);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <motion.h1
+          variants={fadeIn('down', 0.2)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='text-center text-6xl text-green-700 cursor-pointer'
+          onClick={handleAudioPlay} 
+        >
+          Save The World
+        </motion.h1>
+        <audio id="save">
+          <source src='/Save.mp3' />
+        </audio>
+      </div>
+    </div>
+  );
+}
